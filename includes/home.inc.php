92c94018e6423c78f989eb29cf822c9b45f499ed
TooHighDude <nataro112@gmail.com>
2024-12-09 14:23:47 +0100

    Home: lay-out
    completed the lay-out for the home page

--- includes/home.inc.php
+++ includes/home.inc.php
@@ -1,8 +1,8 @@
 <section>
     <div class="absolute top-0 parallax w-[100%]"></div>
     <img src="img/avatar.svg" alt="avatar" class="absolute top-[3%] left-[40.5%]">
-    <img src="img/right.svg" alt="right_background" class="absolute top-[13%]">
-    <img src="img/left.svg" alt="left_background" class="absolute top-[16.5%]">
+    <img src="img/right.svg" alt="right_background" class="absolute top-[13.2%]">
+    <img src="img/left.svg" alt="left_background" class="absolute top-[16.7%]">
     <img src="img/arrow.svg" alt="arrow" class="absolute top-[54%] left-[50%]">
     <div class="bg-[#314326] h-[100vh] w-[100%]"></div>
 </section>
