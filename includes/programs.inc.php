4412342fef70945ee9cc2f25f2f679661096120b
TooHighDude <nataro112@gmail.com>
2024-12-12 11:50:40 +0100

    Programs: lay-out
    Added the screen tot the programs page and added the first slide

--- includes/programs.inc.php
+++ includes/programs.inc.php
@@ -1,8 +1,8 @@
 <section>
-    <div class="absolute top-0 left-[20%] max-h-[100vh]">
-        <img src="img/programs/screen.svg" alt="screen">
+    <div class="absolute top-0 w-[100%] max-h-[100vh]">
+        <img src="img/programs/screen.svg" alt="screen" class="w-[100%] max-h-[100vh]">
     </div>
-    <div class="absolute top-0 left-[20%] w-[50%] max-h-[100vh]">
-        <img src="img/programs/pp-mm.svg" alt="pp-mm">
+    <div class="absolute top-[19.1%] left-[29.3%] w-[41.5%]">
+        <img src="img/programs/pp-mm.png" alt="pp-mm" class="h-[40vh] w-[99.5%] rounded-tl-xl rounded-tr-xl">
     </div>
 </section>
