87f0198ec0835875ff2d10f13659dd1c6f42708d
TooHighDude <nataro112@gmail.com>
2024-12-23 15:27:15 +0100

    finished: Mywork: card lay-out

--- includes/programs.inc.php
+++ includes/programs.inc.php
@@ -4,57 +4,31 @@
             <h1> Mijn werk </h1>
         </div>
         <div class="flex justify-center items-center p-5 gap-10" id="cardContainer">
+            <button onclick="nextCard()"> < </button>
 
-            <button onclick="nextCard()">
-                < </button>
+            <div class="block h-[40vh] max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <img class="w-[30vw] h-[20Vh]" src="" alt="" id="leftImage">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white" id="leftTitle"></h5>
+                <p class="font-normal text-gray-700 dark:text-gray-400" id="leftSubtext"></p>
+            </div>
 
-                    <a href="#"
-                        class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                        <img class="size-36" src="" alt="" id="leftImage">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white" id="leftTitle">
-                            Noteworthy technology
-                            acquisitions 2021
-                        </h5>
-                        <p class="font-normal text-gray-700 dark:text-gray-400" id="leftSubtext">
-                            Here are the biggest enterprise technology
-                            acquisitions of 2021 so far, in reverse chronological order.
-                        </p>
-                    </a>
+            <div class="block h-[40vh] max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <img class="w-[30vw] h-[20Vh]" src="" alt="" id="centerImage">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white" id="centerTitle"></h5>
+                <p class="font-normal text-gray-700 dark:text-gray-400" id="centerSubtext"></p>
+            </div>
 
+            <div class="block h-[40vh] max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <img class="w-[30vw] h-[20Vh]" src="" alt="" id="rightImage">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white" id="rightTitle"></h5>
+                <p class="font-normal text-gray-700 dark:text-gray-400" id="rightSubtext"></p>
+            </div>
 
-                    <a href="#"
-                        class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                        <img src="" alt="" id="centerImage">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-                            id="centerTitle">
-                            Noteworthy technology
-                            acquisitions 2021
-                        </h5>
-                        <p class="font-normal text-gray-700 dark:text-gray-400" id="centerSubtext">
-                            Here are the biggest enterprise technology
-                            acquisitions of 2021 so far, in reverse chronological order.
-                        </p>
-                    </a>
-
-
-                    <a href="#"
-                        class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                        <img src="" alt="" id="rightImage">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-                            id="rightTitle">
-                            Noteworthy technology
-                            acquisitions 2021
-                        </h5>
-                        <p class="font-normal text-gray-700 dark:text-gray-400" id="rightSubtext">
-                            Here are the biggest enterprise technology
-                            acquisitions of 2021 so far, in reverse chronological order.
-                        </p>
-                    </a>
-
-                    <script src="js/Card.js"></script>
-                    <script src="js/Carrousel.js"></script>
-                    <script src="js/myWork.js"></script>
-                    <button onclick="prevCard()"> > </button>
+            <button onclick="prevCard()"> > </button>
         </div>
     </div>
 </section>
+
+<script src="js/mywork/Card.js"></script>
+<script src="js/mywork/Carrousel.js"></script>
+<script src="js/mywork/myWork.js"></script>
