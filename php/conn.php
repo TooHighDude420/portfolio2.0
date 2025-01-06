9860dbe71174211655f75fb93ed58da9bc4941a1
TooHighDude420 <toohighdudepruducts@gmail.com>
2025-01-06 14:31:58 +0100

    added gitignore

--- php/conn.php
+++ php/conn.php
@@ -1,12 +1,12 @@
 <?php
 
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$dbname = "portfolio";
+$host = "localhost";
+$username = "u195361p354045_admin";
+$password = "Wacht@Database112";
+$dbname = "u195361p354045_portfolio";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
